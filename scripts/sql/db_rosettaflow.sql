1c670c590ffbb0c2713c43e6e7ea029975eb4db0
qudong <qudong123@126.com>
2021-09-09 13:28:08 +0800

    数据库user表字段调整

--- scripts/sql/db_rosettaflow.sql
+++ scripts/sql/db_rosettaflow.sql
@@ -7,13 +7,15 @@ DROP TABLE IF EXISTS `t_user`;
 CREATE TABLE `t_user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '用户ID(自增长)',
   `user_name` varchar(64) NOT NULL COMMENT '用户名',
-  `address` varchar(64)  NOT NULL COMMENT '用户钱包地址',
-  `status` tinyint(4) NOT NULL DEFAULT 1 COMMENT '状态: 0-无效，1- 有效',
+  `address` varchar(64) NOT NULL COMMENT '用户钱包地址',
+  `status` tinyint(4) NOT NULL DEFAULT '1' COMMENT '状态: 0-无效，1- 有效',
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_ADDRESS` (`address`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户表';
+  UNIQUE KEY `UK_ADDRESS` (`address`) USING BTREE COMMENT '用户地址唯一',
+  UNIQUE KEY `UK_USERNAME` (`user_name`) USING BTREE COMMENT '用户名称唯一'
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COMMENT='用户表';
+
 
 -- ----------------------------
 -- Table structure for `t_org`
