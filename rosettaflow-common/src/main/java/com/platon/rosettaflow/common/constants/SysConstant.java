be68d7dd558f73d13ec04f861d7c4f80fe3f66c3
侯壮 <15736969819@163.com>
2021-09-14 10:45:20 +0800

    登出接口去调登录校验

--- rosettaflow-common/src/main/java/com/platon/rosettaflow/common/constants/SysConstant.java
+++ rosettaflow-common/src/main/java/com/platon/rosettaflow/common/constants/SysConstant.java
@@ -67,8 +67,10 @@ public class SysConstant {
     /**
      * 不需要用户登录可以访问的接口
      */
-    public static final String[] LOGIN_URIS = {"user/login",
+    public static final String[] LOGIN_URIS = {
             "user/getLoginNonce",
+            "user/login",
+            "user/logout",
             "swagger",
             "error",
             "api-docs",
