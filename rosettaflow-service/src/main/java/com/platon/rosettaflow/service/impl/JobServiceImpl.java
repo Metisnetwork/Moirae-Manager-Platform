6bcef97cf2686a682cf4fc0da2f72954d305c564
qudong <qudong123@126.com>
2021-10-20 11:21:39 +0800

    调整代码格式

--- rosettaflow-service/src/main/java/com/platon/rosettaflow/service/impl/JobServiceImpl.java
+++ rosettaflow-service/src/main/java/com/platon/rosettaflow/service/impl/JobServiceImpl.java
@@ -25,12 +25,10 @@ import com.platon.rosettaflow.service.IWorkflowService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import javax.annotation.Resource;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
-import java.util.function.Consumer;
 
 import static cn.hutool.core.date.DateTime.now;
 
