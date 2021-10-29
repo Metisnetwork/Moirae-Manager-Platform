d79841bbf08bc15c94f595501b894096be19118b
hudenian <hudenian@163.com>
2021-10-29 12:08:29 +0800

    task sync time adjust

--- rosettaflow-quartz/src/main/java/com/moirae/rosettaflow/task/WorkflowNodeStatusTask.java
+++ rosettaflow-quartz/src/main/java/com/moirae/rosettaflow/task/WorkflowNodeStatusTask.java
@@ -40,7 +40,7 @@ public class WorkflowNodeStatusTask {
     /**
      * 查询 当前时间之前5小时的正在运行的数据
      */
-    private static final int BEFORE_HOUR = -5;
+    private static final int BEFORE_HOUR = -12;
 
     @Resource
     private GrpcTaskService grpcTaskService;
