97522ff0d0a66699cafc450a18f61b506baac316
hudenian <hudenian@163.com>
2021-11-05 11:29:41 +0800

    adjust test db connect

--- rosettaflow-controller/src/main/java/com/moirae/rosettaflow/RosettaFlowApplication.java
+++ rosettaflow-controller/src/main/java/com/moirae/rosettaflow/RosettaFlowApplication.java
@@ -3,6 +3,7 @@ package com.moirae.rosettaflow;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * @author admin
  * @date 2021/7/20
  */
+@EnableAsync
 @EnableScheduling
 @SpringBootApplication
 @EnableTransactionManagement
