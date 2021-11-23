dbf69c84d76577c71f88e2180ca48ca17f201dc8
qudong <qudong123@126.com>
2021-11-23 19:17:36 +0800

    调整资产算法列表新增字段

--- rosettaflow-controller/src/main/java/com/moirae/rosettaflow/vo/algorithm/AlgorithmListVo.java
+++ rosettaflow-controller/src/main/java/com/moirae/rosettaflow/vo/algorithm/AlgorithmListVo.java
@@ -21,7 +21,21 @@ public class AlgorithmListVo {
     @ApiModelProperty(value = "算法描述")
     private String algorithmDesc;
 
-    @ApiModelProperty(value = "算法类型")
+    @ApiModelProperty(value = "算法类型：1-统计分析,2-特征工程,3-机器学习")
     private Byte algorithmType;
+
+    @ApiModelProperty(value = "作者")
+    private String author;
+
+    @ApiModelProperty(value = "支持协同方最小数量")
+    private Long minNumbers;
+
+    @ApiModelProperty(value = "支持语言,多个以\",\"进行分隔")
+    private String supportLanguage;
+
+    @ApiModelProperty(value = "算法代码")
+    private String calculateContractCode;
+
+
 
 }
