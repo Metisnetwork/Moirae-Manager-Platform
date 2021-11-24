774ca31549ac37974e834c666ecf96fbb9c740ec
qudong <qudong123@126.com>
2021-11-24 10:59:46 +0800

    算法列表增加分页

--- rosettaflow-mapper/src/main/java/com/moirae/rosettaflow/mapper/AlgorithmMapper.java
+++ rosettaflow-mapper/src/main/java/com/moirae/rosettaflow/mapper/AlgorithmMapper.java
@@ -1,6 +1,7 @@
 package com.moirae.rosettaflow.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.moirae.rosettaflow.dto.AlgorithmDto;
 import com.moirae.rosettaflow.mapper.domain.Algorithm;
 import org.apache.ibatis.annotations.Param;
@@ -15,10 +16,11 @@ public interface AlgorithmMapper extends BaseMapper<Algorithm> {
     /**
      * 查询算法列表
      *
+     * @param page 算法分页
      * @param algorithmName 算法名称
      * @return 算法列表
      */
-    List<AlgorithmDto> queryAlgorithmList(@Param(value = "algorithmName") String algorithmName);
+    IPage<AlgorithmDto> queryAlgorithmList(@Param("page") IPage<AlgorithmDto> page, @Param(value = "algorithmName") String algorithmName);
 
     /**
      * 查询算法树列表
