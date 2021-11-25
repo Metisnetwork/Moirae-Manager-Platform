dcb7bcb1d31c53a881529430a8a40c3701d12131
hudenian <hudenian@163.com>
2021-11-25 17:59:48 +0800

    download rpc interface adjust

--- rosettaflow-grpc-client/src/main/java/com/moirae/rosettaflow/grpc/client/DataProviderServiceClient.java
+++ rosettaflow-grpc-client/src/main/java/com/moirae/rosettaflow/grpc/client/DataProviderServiceClient.java
@@ -1,6 +1,7 @@
 package com.moirae.rosettaflow.grpc.client;
 
 
+import cn.hutool.core.bean.BeanUtil;
 import com.google.protobuf.ByteString;
 import com.moirae.rosettaflow.common.enums.ErrorMsg;
 import com.moirae.rosettaflow.common.enums.RespCodeEnum;
@@ -8,14 +9,15 @@ import com.moirae.rosettaflow.common.enums.TaskDownloadStatusEnum;
 import com.moirae.rosettaflow.common.exception.BusinessException;
 import com.moirae.rosettaflow.grpc.data.provider.req.dto.DownloadRequestDto;
 import com.moirae.rosettaflow.grpc.data.provider.resp.dto.DownloadReplyResponseDto;
-import com.moirae.rosettaflow.grpc.service.*;
+import com.moirae.rosettaflow.grpc.service.DataProviderGrpc;
+import com.moirae.rosettaflow.grpc.service.DownloadReply;
+import com.moirae.rosettaflow.grpc.service.DownloadRequest;
 import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Component;
-import java.util.ArrayList;
+
 import java.util.Iterator;
-import java.util.List;
 
 /**
  * @author admin
@@ -27,73 +29,29 @@ import java.util.List;
 public class DataProviderServiceClient {
 
     @GrpcClient("carrier-grpc-server")
-    DataProviderGrpc.DataProviderStub dataProviderStub;
-
+    DataProviderGrpc.DataProviderBlockingStub dataProviderBlockingStub;
 
 
     /**
      * 下载任务结果
+     *
      * @param requestDto : 任务结果下载入参
      * @return List<DownloadReplyResponseDto> : 任务结果数据文件
      */
     public DownloadReplyResponseDto getTaskDownload(DownloadRequestDto requestDto) {
         DownloadReplyResponseDto responseDto = new DownloadReplyResponseDto();
         DownloadRequest downloadRequest = DownloadRequest.newBuilder()
-                        .setFilePath(requestDto.getFilePath())
-                        .putOptions("compress", requestDto.getCompress().get("compress"))
-                        .build();
-        dataProviderStub.downloadData(downloadRequest, new StreamObserver<DownloadReply>() {
+                .setFilePath(requestDto.getFilePath())
+                .putOptions("compress", requestDto.getCompress().get("compress"))
+                .build();
 
-            @Override
-            public void onNext(DownloadReply downloadReply) {
-                if (downloadReply.hasStatus()) {
-                    int status = downloadReply.getStatus().getNumber();
-                    if (status == TaskDownloadStatusEnum.Failed.getValue()) {
-                        log.info("download task result failed....,filePath:{}",requestDto.getFilePath());
-                        throw new BusinessException(RespCodeEnum.BIZ_FAILED, ErrorMsg.METADATA_RESULT_DOWNLOAD_FAIL.getMsg());
-                    }
-                    if (downloadReply.hasContent()) {
-                        ByteString downloadContent = downloadReply.getContent();
-                        responseDto.setContent(downloadContent.toByteArray());
-                        responseDto.setDownloadStatus(status);
-                    }
-
-                    switch (status) {
-                        case 0:
-                            log.info("download task result start,filePath:{}",requestDto.getFilePath());
-                            break;
-                        case 1:
-                            log.info("download task result finish,filePath:{}",requestDto.getFilePath());
-                            break;
-                        case 2:
-                            log.info("download task result cancel,filePath:{}",requestDto.getFilePath());
-                            break;
-                        default:
-                            log.info("download task result status error,filePath:{}",requestDto.getFilePath());
-                            throw new BusinessException(RespCodeEnum.BIZ_FAILED, ErrorMsg.METADATA_RESULT_DOWNLOAD_ERROR.getMsg());
-                    }
-                } else {
-                    log.info("download task result status error,filePath:{}",requestDto.getFilePath());
-                    throw new BusinessException(RespCodeEnum.BIZ_FAILED, ErrorMsg.METADATA_RESULT_DOWNLOAD_ERROR.getMsg());
-                }
-            }
-
-            @Override
-            public void onError(Throwable throwable) {
-                log.error("Data result download error, filePath:{}, reason:{}", requestDto.getFilePath(), throwable.getMessage());
-                throw new BusinessException(RespCodeEnum.BIZ_FAILED, ErrorMsg.METADATA_RESULT_DOWNLOAD_ERROR.getMsg());
-            }
-
-            @Override
-            public void onCompleted() {
-                log.error("Data result download completed, filePath:{}", requestDto.getFilePath());
-            }
-        });
+        Iterator<DownloadReply> downloadReplyIterator = dataProviderBlockingStub.downloadData(downloadRequest);
+        while (downloadReplyIterator.hasNext()){
+            DownloadReply downloadReply = downloadReplyIterator.next();
+            BeanUtil.copyProperties(downloadReply,responseDto);
+        }
         return responseDto;
     }
-
-
-
 
 
 }
