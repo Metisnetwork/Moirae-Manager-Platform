9552f969555a375a2d508d40fd3ef2b030e727fc
hudenian <hudenian@163.com>
2021-12-14 15:11:32 +0800

    add sync organization nodeid

--- rosettaflow-service/src/main/java/com/moirae/rosettaflow/service/NetManager.java
+++ rosettaflow-service/src/main/java/com/moirae/rosettaflow/service/NetManager.java
@@ -1,5 +1,8 @@
 package com.moirae.rosettaflow.service;
 
+import com.moirae.rosettaflow.common.enums.ErrorMsg;
+import com.moirae.rosettaflow.common.enums.RespCodeEnum;
+import com.moirae.rosettaflow.common.exception.BusinessException;
 import com.moirae.rosettaflow.grpc.identity.dto.NodeIdentityDto;
 import com.moirae.rosettaflow.grpc.service.GrpcAuthService;
 import com.moirae.rosettaflow.mapper.domain.Organization;
@@ -61,6 +64,13 @@ public class NetManager {
         }
     }
 
+    /**
+     * 根据ip 与 port 组装channel
+     *
+     * @param ip   组织ip
+     * @param port 组织端口
+     * @return channel
+     */
     public ManagedChannel assemblyChannel(String ip, String port) {
         return ManagedChannelBuilder
                 .forAddress(ip, Integer.parseInt(port))
@@ -69,8 +79,43 @@ public class NetManager {
                 .build();
     }
 
+    /**
+     * 根据identityId获取channel
+     *
+     * @param identityId identityId
+     * @return channel
+     */
     public ManagedChannel getChannel(String identityId) {
-        return channelMap.get(identityId);
+        ManagedChannel managedChannel = channelMap.get(identityId);
+        if (managedChannel == null) {
+            managedChannel = loadOrganizationByIdentityId(identityId);
+        }
+        return managedChannel;
+    }
+
+    /**
+     * 根据identityId加载机构channel
+     */
+    public ManagedChannel loadOrganizationByIdentityId(String identityId) {
+        Organization organization = organizationService.getByIdentityId(identityId);
+        if (null == organization) {
+            log.error("Can not find organization by identityId:{}", identityId);
+            throw new BusinessException(RespCodeEnum.BIZ_EXCEPTION, ErrorMsg.ORGANIZATION_NOT_EXIST.getMsg());
+        }
+
+        List<NodeIdentityDto> nodeIdentityDtoList = grpcAuthService.getIdentityList();
+        for (NodeIdentityDto nodeIdentityDto : nodeIdentityDtoList) {
+            if (organization.getIdentityId().equals(nodeIdentityDto.getIdentityId())) {
+                organization.setNodeId(nodeIdentityDto.getNodeId());
+                break;
+            }
+        }
+        organizationService.updateById(organization);
+        ManagedChannel channel = assemblyChannel(organization.getIdentityIp(), organization.getIdentityPort());
+
+        channelMap.put(organization.getIdentityId(), channel);
+        identityIdIpPortMap.put(organization.getIdentityId(), organization.getIdentityIp() + delimiter + organization.getIdentityPort());
+        return channel;
     }
 
     /**
