41d6e1e3746148c1cda6051be99dc42a2d44daff
侯壮 <15736969819@163.com>
2021-12-23 16:46:04 +0800

    提示语修改

--- rosettaflow-common/src/main/java/com/moirae/rosettaflow/common/enums/ErrorMsg.java
+++ rosettaflow-common/src/main/java/com/moirae/rosettaflow/common/enums/ErrorMsg.java
@@ -42,8 +42,8 @@ public enum ErrorMsg {
     ALGORITHM_CODE_NOT_NOT_EXISTS("项目模板对应的工作流节点代码不存在", "Project template algorithm code not exists"),
     USER_ADMIN_PERMISSION_ERROR("您不是当前项目管理员，无操作权限！", "You are not this project administrator, No operation authority"),
     USER_NOT_PERMISSION_ERROR("您是项目查看者，暂无编辑权限！", "You are a project viewer and have no editing rights"),
-    USER_ADMIN_UPDATE_ERROR("不能更改最后一个管理员", "Cannot update the last administrator"),
-    USER_ADMIN_DELETE_ERROR("不能删除自己的权限！", "Cannot delete own permissions!"),
+    USER_ADMIN_UPDATE_ERROR("不能更改最后一个管理员权限！", "Cannot change the last admin permissions"),
+    USER_ADMIN_DELETE_ERROR("不能将自己的项目权限删除！", "Cannot delete own project permissions!"),
     USER_ACCESS_PERMISSION_ERROR("您无权访问当前项目！", "You don't have permission to access the current project!"),
 
     /**
