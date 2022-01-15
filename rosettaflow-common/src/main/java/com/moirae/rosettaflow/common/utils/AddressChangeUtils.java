23c14a4b9d9ab3c403a3f6f8be0f04520886762e
dai chen <chenxiaodai@126.com>
2022-01-15 19:14:52 +0800

    shield main

--- rosettaflow-common/src/main/java/com/moirae/rosettaflow/common/utils/AddressChangeUtils.java
+++ rosettaflow-common/src/main/java/com/moirae/rosettaflow/common/utils/AddressChangeUtils.java
@@ -155,20 +155,20 @@ public class AddressChangeUtils {
     /**
      * 合约前缀转换
      */
-    public static void main(String[] args) {
-        String addr = "lat1krh2rm747g2j0z6zp5sup06u6ez34fx8y8vd25";
-        String latAddr = AddressChangeUtils.encode("lat", convertBits(Numeric.hexStringToByteArray(addr), 8, 5, true));
-        System.out.println("lat地址为>>>" + latAddr);
-        System.out.println(DataChangeUtils.bytesToHex(Bech32.addressDecode(latAddr)));
-        System.out.println(convert0xAddress("lat1krh2rm747g2j0z6zp5sup06u6ez34fx8y8vd25"));
+    // public static void main(String[] args) {
+    //     String addr = "lat1krh2rm747g2j0z6zp5sup06u6ez34fx8y8vd25";
+    //     String latAddr = AddressChangeUtils.encode("lat", convertBits(Numeric.hexStringToByteArray(addr), 8, 5, true));
+    //     System.out.println("lat地址为>>>" + latAddr);
+    //     System.out.println(DataChangeUtils.bytesToHex(Bech32.addressDecode(latAddr)));
+    //     System.out.println(convert0xAddress("lat1krh2rm747g2j0z6zp5sup06u6ez34fx8y8vd25"));
 
-        List<String> addrList = new ArrayList<>();
-        addrList.add("0x990a168ecee09b8b1abeff3e2b713924e7151f9b");
-        addrList.add("0x5146e07c5157ea8ad00bb0c196e61671f4af85f0");
-        addrList.add("0xc1cdaeecda5744bbe1a97ff3f311353f9455e2f0");
-        addrList.add("0x1000000000000000000000000000000000000004");
-        addrList.add("0x1000000000000000000000000000000000000005");
-        addrList.add("0x0000000000000000000000000000000000000001");
-        addrList.forEach(a -> System.out.println(a + "转换后的钱包地址>>>" + AddressChangeUtils.encode("lat", convertBits(Numeric.hexStringToByteArray(a), 8, 5, true))));
-    }
+    //     List<String> addrList = new ArrayList<>();
+    //     addrList.add("0x990a168ecee09b8b1abeff3e2b713924e7151f9b");
+    //     addrList.add("0x5146e07c5157ea8ad00bb0c196e61671f4af85f0");
+    //     addrList.add("0xc1cdaeecda5744bbe1a97ff3f311353f9455e2f0");
+    //     addrList.add("0x1000000000000000000000000000000000000004");
+    //     addrList.add("0x1000000000000000000000000000000000000005");
+    //     addrList.add("0x0000000000000000000000000000000000000001");
+    //     addrList.forEach(a -> System.out.println(a + "转换后的钱包地址>>>" + AddressChangeUtils.encode("lat", convertBits(Numeric.hexStringToByteArray(a), 8, 5, true))));
+    // }
 }
--- rosettaflow-common/src/main/java/com/moirae/rosettaflow/common/utils/WalletSignUtils.java
+++ rosettaflow-common/src/main/java/com/moirae/rosettaflow/common/utils/WalletSignUtils.java
@@ -93,29 +93,29 @@ public class WalletSignUtils {
         return "0x" + DatatypeConverter.printHexBinary(bytesValue).toLowerCase();
     }
 
-    public static void main(String[] args) {
-        String uuid = "ec1ab67261cd4a0387223c9952a3f455";
-        //登录签名
-        String json = "{\"domain\":{\"name\":\"Moirae\"},\"message\":{\"key\":\"{ec1ab67261cd4a0387223c9952a3f455}\",\"desc\":\"Welcome to Moirae!\"},\"primaryType\":\"Login\",\"types\":{\"EIP712Domain\":[{\"name\":\"name\",\"type\":\"string\"}],\"Login\":[{\"name\":\"key\",\"type\":\"string\"},{\"name\":\"desc\",\"type\":\"string\"}]}}";
-        //启动工作流签名
-//        String json = "{\"domain\":{\"name\":\"Moirae\"},\"message\":{\"address\":\"d6a151c0703d47e6baa068700e8c5381\"},\"primaryType\":\"sign\",\"types\":{\"EIP712Domain\":[{\"name\":\"name\",\"type\":\"string\"}],\"sign\":[{\"name\":\"key\",\"type\":\"string\"}]}}";
+//     public static void main(String[] args) {
+//         String uuid = "ec1ab67261cd4a0387223c9952a3f455";
+//         //登录签名
+//         String json = "{\"domain\":{\"name\":\"Moirae\"},\"message\":{\"key\":\"{ec1ab67261cd4a0387223c9952a3f455}\",\"desc\":\"Welcome to Moirae!\"},\"primaryType\":\"Login\",\"types\":{\"EIP712Domain\":[{\"name\":\"name\",\"type\":\"string\"}],\"Login\":[{\"name\":\"key\",\"type\":\"string\"},{\"name\":\"desc\",\"type\":\"string\"}]}}";
+//         //启动工作流签名
+// //        String json = "{\"domain\":{\"name\":\"Moirae\"},\"message\":{\"address\":\"d6a151c0703d47e6baa068700e8c5381\"},\"primaryType\":\"sign\",\"types\":{\"EIP712Domain\":[{\"name\":\"name\",\"type\":\"string\"}],\"sign\":[{\"name\":\"key\",\"type\":\"string\"}]}}";
 
-        try {
-            json = StrUtil.format(json, uuid);
-            System.out.println("加密的json字符串为>>>" + json);
-            Credentials credentials = Credentials.create("f366d751f2b18ee39404e6be657dab7cd0e31da62ff7dbb49ddb8da15521e8ac");
-            System.out.println("钱包hrp地址>>>" + credentials.getAddress());
-            System.out.println("钱包0x地址>>>" + AddressChangeUtils.convert0xAddress(credentials.getAddress()));
+//         try {
+//             json = StrUtil.format(json, uuid);
+//             System.out.println("加密的json字符串为>>>" + json);
+//             Credentials credentials = Credentials.create("f366d751f2b18ee39404e6be657dab7cd0e31da62ff7dbb49ddb8da15521e8ac");
+//             System.out.println("钱包hrp地址>>>" + credentials.getAddress());
+//             System.out.println("钱包0x地址>>>" + AddressChangeUtils.convert0xAddress(credentials.getAddress()));
 
-            System.out.println("签名结果>>>" + signTypedDataV4(json, credentials.getEcKeyPair()));
+//             System.out.println("签名结果>>>" + signTypedDataV4(json, credentials.getEcKeyPair()));
 
-            System.out.println("验证签名结果>>>" + verifyTypedDataV4(
-                    json,
-                    "0xfb71e85fb91026933d0057c263b549197e4d93e8a955728892f3ae9cd4cc6c4973c235ecc4dc2bf6927006b53f42bc3f56110649f040357489f41b3cd9c10af71c",
-                    "lax1j0q78v8g9l94pkwyk3tgk0vfy5ukdz3q2e7wqa"));
+//             System.out.println("验证签名结果>>>" + verifyTypedDataV4(
+//                     json,
+//                     "0xfb71e85fb91026933d0057c263b549197e4d93e8a955728892f3ae9cd4cc6c4973c235ecc4dc2bf6927006b53f42bc3f56110649f040357489f41b3cd9c10af71c",
+//                     "lax1j0q78v8g9l94pkwyk3tgk0vfy5ukdz3q2e7wqa"));
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
+//     }
 }
