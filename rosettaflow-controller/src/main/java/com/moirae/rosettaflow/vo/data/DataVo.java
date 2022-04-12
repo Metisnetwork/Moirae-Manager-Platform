86c78e82621a5fe3916354da355f4c41108b1380
chenxiaodai <chenxiaodai@126.com>
2022-04-12 17:42:10 +0800

    0.4.0开发-数据相关接口调整

--- rosettaflow-controller/src/main/java/com/moirae/rosettaflow/vo/data/DataVo.java
+++ rosettaflow-controller/src/main/java/com/moirae/rosettaflow/vo/data/DataVo.java
@@ -6,6 +6,8 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.util.Date;
+
 @EqualsAndHashCode(callSuper = true)
 @Data
 @ApiModel(value = "元数据信息")
@@ -20,6 +22,12 @@ public class DataVo extends BaseOrgVo {
     @ApiModelProperty(value = "数据凭证名称")
     private String tokenName;
 
+    @ApiModelProperty(value = "数据凭证符号")
+    private String tokenSymbol;
+
     @ApiModelProperty(value = "数据凭证价格")
     private String tokenPrice;
+
+    @ApiModelProperty(value = "发布时间，精确到毫秒")
+    private Date publishedAt;
 }
--- rosettaflow-controller/src/main/java/com/moirae/rosettaflow/vo/data/UserDataVo.java
+++ rosettaflow-controller/src/main/java/com/moirae/rosettaflow/vo/data/UserDataVo.java
@@ -13,9 +13,6 @@ public class UserDataVo extends DataVo {
     @ApiModelProperty(value = "数据凭证余额")
     private String tokenBalance;
 
-    @ApiModelProperty(value = "数据凭证符号")
-    private String tokenSymbol;
-
     @ApiModelProperty(value = "数据凭证精度")
     private Long tokenDecimal;
 }
