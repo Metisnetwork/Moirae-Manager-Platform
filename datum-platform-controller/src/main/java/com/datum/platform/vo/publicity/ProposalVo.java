e6d1b67f7500887a7da747d5e06962df0675bcc6
chenxiaodai <chenxiaodai@126.com>
2022-08-23 16:04:16 +0800

    公式中心联调

--- datum-platform-controller/src/main/java/com/datum/platform/vo/publicity/ProposalVo.java
+++ datum-platform-controller/src/main/java/com/datum/platform/vo/publicity/ProposalVo.java
@@ -1,5 +1,6 @@
 package com.datum.platform.vo.publicity;
 
+import com.datum.platform.mapper.enums.ProposalTypeEnum;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -19,12 +20,17 @@ public class ProposalVo {
     private String candidateIdentityId;
     @ApiModelProperty(value = "候选的组织名称")
     private String candidateNodeName;
-    @ApiModelProperty(value = "提案的类型 1-增加委员会成员; 2-剔除委员会成员")
-    private Integer type;
     @ApiModelProperty(value = "投票开始时间")
     private Date voteBeginTime;
     @ApiModelProperty(value = "投票结束时间")
     private Date voteEndTime;
     @ApiModelProperty(value = "投票状态 0-未开始; 1-投票中; 2-投票通过; 3-投票未通过; 6-已撤销")
     private Integer status;
+
+
+    private ProposalTypeEnum type;
+    @ApiModelProperty(value = "提案类型, 1-增加委员会成员; 2-剔除委员会成员; 3-委员会成员退出")
+    public Integer getType(){
+        return type.getValue();
+    }
 }
