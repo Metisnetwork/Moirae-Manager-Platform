d77728a87614acb9b76c4b1aa52e597196d1258b
chenxiaodai <chenxiaodai@126.com>
2022-10-28 11:17:59 +0800

    向导模式下工作流设置预测输入时，返回训练的输入

--- datum-platform-service/src/main/java/com/datum/platform/service/impl/WorkflowServiceImpl.java
+++ datum-platform-service/src/main/java/com/datum/platform/service/impl/WorkflowServiceImpl.java
@@ -583,7 +583,7 @@ public class WorkflowServiceImpl implements WorkflowService {
 
     private String getPreTaskInput(Long workflowId, Long workflowVersion, Integer task2Step) {
         if(task2Step > 1){
-            WorkflowTask workflowTask = workflowTaskManager.getByStep(workflowId, workflowVersion, task2Step);
+            WorkflowTask workflowTask = workflowTaskManager.getByStep(workflowId, workflowVersion, task2Step - 1);
             if(workflowTask != null && StringUtils.isNotBlank(workflowTask.getIdentityId())){
                 return workflowTask.getIdentityId();
             }
